83bdacd72f8d8ab59a5d0ca8d26f48a2da3704e7
Fabio Gomez <fabiogomez@us.ibm.com>
2018-04-26 16:55:54 -0500

    - Deleted some scripts that were either no longer needed or that were put into the READMEs themselves

--- scripts/setenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Set shared variables
-export progname=dal
-export dalserv=src/main/java/inventory/ws/DALService.java
-export k8cluster=green2-cluster.icp
-export namespace=browncompute
