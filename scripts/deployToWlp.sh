948e8b3c09231d5bf68db4a1340ea46a55011d92
Jerome Boyer <boyerje@us.ibm.com>
2018-05-02 11:24:03 -0700

    Script to upload to WLP on premise

--- scripts/deployToWlp.sh
+++ scripts/deployToWlp.sh
@@ -9,9 +9,9 @@ if [[ "$h" == "brownlibertyappserver.localdomain" ]]; then
 else
    ./exp admin01 scp ../build/libs/*.war admin@172.16.249.79:~/IBM/wlp/usr/servers/appServer/apps
    sleep 10
-   ./exp admin01 ssh admin@172.16.254.44 ~/IBM/wlp/bin/server stop appServer
+   ./exp admin01 ssh admin@172.16.249.79 ~/IBM/wlp/bin/server stop appServer
    sleep 5
-   ./exp admin01 ssh admin@172.16.254.44 ~/IBM/wlp/bin/server start appServer
+   ./exp admin01 ssh admin@172.16.249.79 ~/IBM/wlp/bin/server start appServer
 fi
 
 
