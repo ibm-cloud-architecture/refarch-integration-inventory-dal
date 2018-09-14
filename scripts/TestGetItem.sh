7814c2f3b5d7be8bddd977d753c4846d38d01671
Jerome Boyer <boyerje@us.ibm.com>
2018-09-14 08:11:01 -0700

    update persitence to remove openjpa properties

--- scripts/TestGetItem.sh
+++ scripts/TestGetItem.sh
@@ -1,1 +1,3 @@
-curl -X POST --header "Content-Type: text/xml;charset=UTF-8" --header "/ws.inventory/DALService/itemById" --data @soap-env.xml http://green.csplab.local/inventory/ws
+# curl -X POST --header "Content-Type: text/xml;charset=UTF-8" --header "/ws.inventory/DALService/itemById" --data @soap-env.xml http://green.csplab.local/inventory/ws
+curl -X POST --header "Content-Type: text/xml;charset=UTF-8" --header "/ws.inventory/DALService/itemById" --data @soap-env.xml http://169.45.191.91:9080/inventory/ws
+
